df514688fa325ed1b098eaff973db9f08b1c7a73
Joel Parker Henderson <joel@joelparkerhenderson.com>
2016-01-03 03:06:05 -0700

    Publish

---
+++ README.md
@@ -0,0 +1,7 @@
+# SixArm.com »<br>APM install our favorites for the Atom editor
+
+These are our team's "apm install" commands for a typical system.
+
+Before we begin:
+
+  * Install the Atom editor
---
+++ apm-install-our-favorites.sh
@@ -0,0 +1,47 @@
+apm install Color-picker        `# Press Ctrl-Alt-C to get a colour picker window, insert hex/rgb/hsl values`
+apm install Script              `# Run scripts based on file name, a selection of code, or by line number`
+apm install Sublime-Style-Column-Selection
+apm install activate-power-mode
+apm install atom-alignment      `# A simple key-binding for aligning multi-line and multiple selections`
+apm install atom-beautify       `# Beautify HTML, CSS, SQL, and source code`
+apm install atom-jade           `# Jade language support in Atom`
+apm install block-comment
+apm install color-picker        `# Right click or press CMD-SHIFT-C/CTRL-ALT-C to open it`
+apm install docblockr           `# Automatically adds JSDoc comments to your functions`
+apm install editor-config       `# Helps keep consistent coding styles across projects`
+apm install emmet               `# Abbreviations that expand into HTML/CSS content`
+apm install file-icons          `# Assign file extension icons and colours for improved visuals`
+apm install git-control
+apm install git-log             `# Graph of the commits on a project`
+apm install highlight-selected  `# Highlights the current word selected when double clicking`
+apm install linter              `# A Base Linter with Cow Powers`
+apm install linter-clojure
+apm install linter-csslint
+apm install linter-eslint
+apm install linter-jscs         `# Checks your code style`
+apm install linter-rubocop
+apm install markdown-pdf        `# Convert markdown to pdf, png or jpeg on the fly`
+apm install merge-conflicts
+apm install open-recent
+apm install pigments            `# Display colors`
+apm install rainbow-tabs        `# Adds color to the text of atom tabs, according to file extension (e.g. JS tabs are yellow)`
+apm install todo-show           `# Display your todo, fixme, and other notes in your code`
+apm install build
+apm install seti-icons
+apm install tool-bar
+apm install pdf-view            `# View pdf inside Atom`
+apm install pygments            `# Highlight colors`
+apm install toggle-quotes       `# Toggle quotes from " to ' and vice versa`
+apm install remote-FTP
+apm install angular-styleguide-snippets
+apm install atom-typescript
+apm install git-plus
+apm install gitignore-snippets
+apm install atom-bootstrap4
+apm install mechanical-keyboard-1
+apm install minimap
+apm install atom-material-syntax
+apm install atom-material-ui
+apm install autoclose-html
+apm install revert-buffer
+apm install terminal-plus
